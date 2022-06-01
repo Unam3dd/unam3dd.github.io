8fb60f17b212a313ce4682a63aa4206be848ff4e
Samuel Tales <stales@2G2.42angouleme.fr>
2022-06-01 17:07:10 +0200

    update

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-const term = new Terminal();
+const term = new Terminal({
+	cursorStyle: "block",
+	cursorBlink: true,
+	fontSize:	 18,
+	//rows:		 40,
+	//cols:		 170,
+	theme: {
+		background:	'#4f545c',
+		cursor:		'#ffffff',
+		forground:	'#ff0000',
+	}
+});
 
-var line = "";
-var index = 0;
-var prompt = ">>>"
+var line 		= "";
+var index		= 0;
+var path		= "~";
+const prompt	= `shell:${path}$ `;
+var pos			= 0;
 
 function help_panel(term)
 {
@@ -13,11 +26,30 @@ function help_panel(term)
 function clear_console(term)
 {
 	term.write('\33[H\33[2J');
+	document.getElementById('content').innerHTML = "";
 }
+
+function print_working_directory(term)
+{
+	term.write(`${path}\r\n`);
+}
+
+function fetch_subject(term)
+{
+	console.log(fetch('README.md', {mode: 'no-cors'}).then(obj => {
+		//document.getElementById('content').innerHTML = marked.parse());
+		console.log(obj.text().then(data => {
+			document.getElementById('content').innerHTML = marked.parse(data);
+		}));
+	}));
+}
+
 
 var callbacks = [
 				['clear', clear_console, 'clear console\n'],
 				['help', help_panel, 'show help\n'],
+				['pwd', print_working_directory, 'command test\n'],
+				['fetch', fetch_subject, 'fetch subject\n'],
 				]
 
 function executeCommands(line)
@@ -31,12 +63,20 @@ term.write(prompt + ' ');
 
 term.on('key', (key, ev) => {
 	console.log(ev);
+	if (ev.keyCode == 37) {
+		if (!pos)
+			return ;
+		pos--;
+	}
+	if (ev.keyCode == 39)
+		pos++;
+	if (ev.keyCode == 38 || ev.keyCode == 40)
+		return ;
 	if (ev.keyCode == 13) {
 		term.write("\r\n");
 		executeCommands(line);
 		line = "";
 		term.write(prompt);
-		term.write(' ');
 		index = 0;
 		return ;
 	}
@@ -51,7 +91,7 @@ term.on('key', (key, ev) => {
 	term.write(key);
 });
 
-term.on("paste", (data) => {
+term.on('paste', (data) => {
 	line += data;
 	term.write(data);
 });
